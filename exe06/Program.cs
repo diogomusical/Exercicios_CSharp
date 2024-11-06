0fc1d1c686f3afdad1064d7052ea155f6ff5f2ef
DIOGO COELHO DA SILVA <diogo.musical1@gmail.com>
2024-11-06 13:49:44 +0000

    exercicio06 - Contagem de Votos
    Exercício 06 - Em uma eleição presidencial existem 4 candidatos.
    Os votos são informados através de códigos.
    Os dados para a  contagem de votos à seguinte codificação:
              - 1,2,3,4 - voto nos respectivos candidatos
              - 5 - voto nulo
              - 6 - voto  em Branco
    
    Para terminar o programa deve colocar-se 0.

---
+++ exe06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha o seu candidato introduzindo um número:");
+            Console.WriteLine("\t1 - Candidato 1");
+            Console.WriteLine("\t2 - Candidato 2");
+            Console.WriteLine("\t3 - Candidato 3");
+            Console.WriteLine("\t4 - Candidato 4");
+            Console.WriteLine("\t5 - Voto Nulo");
+            Console.WriteLine("\t6 - Voto Branco");
+            Console.WriteLine("\t0 - PARA SAIR");
+        }
+    }
+}
