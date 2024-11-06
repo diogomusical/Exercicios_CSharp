c89fb28ba34e21173340766be1147f2197236e51
DIOGO COELHO DA SILVA <diogo.musical1@gmail.com>
2024-11-06 13:36:22 +0000

    exe5_Soma_Nr_Positivos

---
+++ exe5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero, total_numero = 0.0, nr_numeros = 0.0;
+
+            do
+            {
+                Console.WriteLine("Introduza o " + $"{nr_numeros+1}" + "º número positivo: ");
+                numero = double.Parse(Console.ReadLine());
+                if (numero >= 0)
+                {
+                    total_numero += numero;
+                    nr_numeros += 1.0;
+                }
+            } while (numero >= 0);
+            Console.WriteLine("MEDIA: " + $"{total_numero/nr_numeros}");
+        }
+    }
+}
