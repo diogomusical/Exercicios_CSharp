55f0425c27633b822c07bb94dc1d3387ed53b181
DIOGO COELHO DA SILVA <diogo.musical1@gmail.com>
2024-11-06 14:17:44 +0000

    exe06

--- exe06/Program.cs
+++ exe06/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +11,43 @@ namespace exe06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Escolha o seu candidato introduzindo um número:");
-            Console.WriteLine("\t1 - Candidato 1");
-            Console.WriteLine("\t2 - Candidato 2");
-            Console.WriteLine("\t3 - Candidato 3");
-            Console.WriteLine("\t4 - Candidato 4");
-            Console.WriteLine("\t5 - Voto Nulo");
-            Console.WriteLine("\t6 - Voto Branco");
-            Console.WriteLine("\t0 - PARA SAIR");
+            int numero;
+            int[] nr_candidatos = new int[6];
+            do
+            {
+                Console.WriteLine("Escolha o seu candidato");
+                Console.WriteLine("\t1 - Candidato 1");
+                Console.WriteLine("\t2 - Candidato 2");
+                Console.WriteLine("\t3 - Candidato 3");
+                Console.WriteLine("\t4 - Candidato 4");
+                Console.WriteLine("\t5 - Voto Nulo");
+                Console.WriteLine("\t6 - Voto Branco");
+                Console.WriteLine("\t0 - PARA SAIR");
+                Console.WriteLine("Introduza um número: ");
+                numero = int.Parse(Console.ReadLine());
+
+             
+                //Contagem dos votos
+                for(int i = 0;i < nr_candidatos.Length; i++)
+                {
+                    if (numero-1 == i)
+                    {
+                        nr_candidatos[i]++;
+                    } ;
+                }
+
+                Console.WriteLine("Número total de votos:");
+
+            } while (numero != 0);
+
+            //Visualização de voto
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Candidato " + $"{i + 1}" + ":" + "\t" + $"{nr_candidatos[i]}");
+            }
+            Console.WriteLine("Número de votos nulos:" + "\t" + $"{nr_candidatos[4]}");
+            Console.WriteLine("Número de votos brancos:" + $"{nr_candidatos[5]}");
+
         }
     }
 }
