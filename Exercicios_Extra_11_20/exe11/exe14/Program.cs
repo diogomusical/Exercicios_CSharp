c5eefb59cb19fcaeb8fe309a93ed9986bdc1f445
DIOGO COELHO DA SILVA <diogo.musical1@gmail.com>
2024-11-07 15:02:59 +0000

    exe14

---
+++ Exercicios_Extra_11_20/exe11/exe14/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            int valor;
+            int nr_valores_pos = 0, nr_valores_neg = 0;
+            double total_valores = 0.0;
+            int nr_valores;
+
+            Console.WriteLine("Introduza o número de valores que pretende introduzir:");
+            nr_valores = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nr_valores; i++)
+            {
+                Console.WriteLine("Introduza o " + $"{i + 1}" + "º número:");
+                valor = int.Parse(Console.ReadLine());
+
+                if(valor >= 0)
+                {
+                    nr_valores_pos++;
+                }
+                else
+                {
+                    nr_valores_neg++;
+                }
+                total_valores += valor;
+            }
+
+            Console.WriteLine("MÉDIA: " + $"{ total_valores / (nr_valores_pos + nr_valores_neg)}");
+
+
+        }
+    }
+}
