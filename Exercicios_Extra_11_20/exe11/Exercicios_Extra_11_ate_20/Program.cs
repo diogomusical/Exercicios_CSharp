8f97598e492aa1bc45e9fb59e80b37d1896303d9
DIOGO COELHO DA SILVA <diogo.musical1@gmail.com>
2024-11-07 11:51:09 +0000

    exe11_Progressao_Aritmetica

---
+++ Exercicios_Extra_11_20/exe11/Exercicios_Extra_11_ate_20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_Extra_11_ate_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inicio, razao , nr, total;
+
+            Console.WriteLine("Introduza o número em que inicia a progressão aritmética :");
+            inicio =  int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduza a razão: ");
+            razao = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduza o número de termos: ");
+            nr = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("1º termo: " + $"{inicio}");
+            total = inicio;
+
+
+            for (int i = 1; i < nr; i++)
+            {
+                Console.WriteLine($"{i + 1}" + "º termo: " + $"{razao * i}");
+                total += razao*i;
+            }
+
+            Console.WriteLine("Total da soma dos termos: " + $"{total}");
+
+        }
+    }
+}
---
+++ Exercicios_Extra_11_20/exe11/Exercicios_Extra_11_ate_20/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Exercicios_Extra_11_ate_20")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercicios_Extra_11_ate_20")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd240eea-b860-4367-bfb7-1bde32b8b3d0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
