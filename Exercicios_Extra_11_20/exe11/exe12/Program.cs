79ed10c4d0326b7a2d583182d9dbe020fb5871b5
DIOGO COELHO DA SILVA <diogo.musical1@gmail.com>
2024-11-07 12:08:26 +0000

    exe11_Tabuada

---
+++ Exercicios_Extra_11_20/exe11/exe12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nr_valores = 5;
+            int[] valores = new int[nr_valores];
+
+            //Carregamento dos valores inseridos pelo utilizador
+            for (int i = 0; i < nr_valores; i++)
+            {
+                Console.WriteLine("Introduza o " + $"{i + 1}" + "º número:");
+                valores[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Visualização dos dados
+            for (int i = 0; i < nr_valores; i++)
+            {
+                Console.WriteLine("\n\n\n");
+                for (int j = 0; j < valores[i]; j++)
+                {
+                    Console.WriteLine("\t" + $"{j+1}" + " x " + $"{valores[i]}" + " = " + $"{(j+1) * valores[i]}");
+                }
+            }
+
+            Console.WriteLine("\n\n FIM DO PROGRAMA");
+
+        }
+    }
+}
